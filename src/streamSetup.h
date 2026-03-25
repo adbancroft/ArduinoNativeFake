2453b9e2c2d8828d78620cd8ea3f819c69ffef36
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-03-25 18:07:47 -0500

    Stream is-a print , so mock the Print base class of a Stream.

--- src/streamSetup.h
+++ src/streamSetup.h
@@ -2,14 +2,18 @@
 #include <map>
 #include <type_traits>
 #include "streamSetupDetail.hpp"
+#include "printSetup.h"
 
 namespace ArduinoNativeFake
 {
     
 template <class TFake>
-static inline void setupStreamFake(fakeit::Mock<TFake> &mock, std::istream &stream)
+static inline void setupStreamFake(fakeit::Mock<TFake> &mock, std::ostream &oStream, std::istream &iStream)
 {
     static_assert(std::is_base_of<Stream, TFake>::value, "Needs to be a class derived from Stream");
+
+    // STream is-a Print
+    setupPrintFake(mock, oStream);
 
     // These are pure virtual in Stream, so don't implement here (they'll be implemented in fakes derived from Stream)
     // virtual int available() = 0;
@@ -42,7 +46,7 @@ static inline void setupStreamFake(fakeit::Mock<TFake> &mock, std::istream &stre
     // reads data from the stream until the target string of the given length is found
     // search terminated if the terminator string is found
     // returns true if target string is found, false if terminated or timed out
-    When(OverloadedMethod(mock, findUntil, bool(const char*, size_t, const char*, size_t))).AlwaysDo([&stream](const char *target, size_t targetLen, const char *terminate, size_t termLen){
+    When(OverloadedMethod(mock, findUntil, bool(const char*, size_t, const char*, size_t))).AlwaysDo([&iStream](const char *target, size_t targetLen, const char *terminate, size_t termLen){
         std::string strTarget(target, StreamDetail::max_strlen(target, targetLen));
         std::string strTerminate(terminate, StreamDetail::max_strlen(terminate, termLen));
 
@@ -50,26 +54,26 @@ static inline void setupStreamFake(fakeit::Mock<TFake> &mock, std::istream &stre
         if (!strTarget.empty() && !strTerminate.empty())
         {
             std::array<std::string, 2> searchTargets = { strTarget, strTerminate };
-            found = StreamDetail::search(searchTargets.begin(), searchTargets.end(), stream);
+            found = StreamDetail::search(searchTargets.begin(), searchTargets.end(), iStream);
         }
         else if (!strTarget.empty()) {
-            found = StreamDetail::search(strTarget, stream);
+            found = StreamDetail::search(strTarget, iStream);
         }
         else if (!strTerminate.empty()) {
-            found = StreamDetail::search(strTerminate, stream);
+            found = StreamDetail::search(strTerminate, iStream);
         } else {
             // Deliberate - do nothing
         }
         return !found.empty();
     });
     
-    When(OverloadedMethod(mock, parseInt, long(LookaheadMode, char))).AlwaysDo([&stream](LookaheadMode mode, char ignore){
+    When(OverloadedMethod(mock, parseInt, long(LookaheadMode, char))).AlwaysDo([&iStream](LookaheadMode mode, char ignore){
         // Create a locale with the custom ignore facet and
         // imbue the stringstream with the custom locale
-        auto oldLocale = stream.imbue(std::locale(std::locale::classic(), new StreamDetail::ignore_char_int_t(mode, ignore)));
+        auto oldLocale = iStream.imbue(std::locale(std::locale::classic(), new StreamDetail::ignore_char_int_t(mode, ignore)));
         std::string i;
-        stream >> i;
-        (void)stream.imbue(oldLocale);
+        iStream >> i;
+        (void)iStream.imbue(oldLocale);
         try { 
             return std::stol(StreamDetail::removeIgnoreChar(i, ignore));
         }
@@ -79,13 +83,13 @@ static inline void setupStreamFake(fakeit::Mock<TFake> &mock, std::istream &stre
         }
     });
 
-    When(OverloadedMethod(mock, parseFloat, float(LookaheadMode, char))).AlwaysDo([&stream](LookaheadMode mode, char ignore){
+    When(OverloadedMethod(mock, parseFloat, float(LookaheadMode, char))).AlwaysDo([&iStream](LookaheadMode mode, char ignore){
         // Create a locale with the custom ignore facet and
         // imbue the stringstream with the custom locale
-        auto oldLocale = stream.imbue(std::locale(std::locale::classic(), new StreamDetail::ignore_char_float_t(mode, ignore)));
+        auto oldLocale = iStream.imbue(std::locale(std::locale::classic(), new StreamDetail::ignore_char_float_t(mode, ignore)));
         std::string i;
-        stream >> i;
-        (void)stream.imbue(oldLocale);
+        iStream >> i;
+        (void)iStream.imbue(oldLocale);
         try { 
             return std::stof(StreamDetail::removeIgnoreChar(i, ignore));
         }
@@ -95,37 +99,37 @@ static inline void setupStreamFake(fakeit::Mock<TFake> &mock, std::istream &stre
         }
     });
 
-    When(OverloadedMethod(mock, readBytes, size_t(char*, size_t))).AlwaysDo([&stream](char *buffer, size_t length){
-        (void)stream.read(buffer, length);
-        return stream.gcount();
+    When(OverloadedMethod(mock, readBytes, size_t(char*, size_t))).AlwaysDo([&iStream](char *buffer, size_t length){
+        (void)iStream.read(buffer, length);
+        return iStream.gcount();
     });
 
-    When(OverloadedMethod(mock, readBytesUntil, size_t(char, char*, size_t))).AlwaysDo([&stream](char terminator, char *buffer, size_t length){
-        char c = stream.get();
+    When(OverloadedMethod(mock, readBytesUntil, size_t(char, char*, size_t))).AlwaysDo([&iStream](char terminator, char *buffer, size_t length){
+        char c = iStream.get();
         size_t index = 0;
         while ((index < length)
             && (c!=std::char_traits<char>::eof())
             && (c!=terminator)) {
             *buffer++ = c;
             ++index;
-            c = stream.get();
+            c = iStream.get();
         }
         return index; // return number of characters, not including null terminator
     });
 
-    When(Method(mock, readString)).AlwaysDo([&stream](){
+    When(Method(mock, readString)).AlwaysDo([&iStream](){
         std::string s;
-        stream >> s;
+        iStream >> s;
         return String(s.c_str());
     });
 
-    When(Method(mock, readStringUntil)).AlwaysDo([&stream](char terminator){
+    When(Method(mock, readStringUntil)).AlwaysDo([&iStream](char terminator){
         std::string s;
-        char c = stream.get();
+        char c = iStream.get();
         while ((c!=std::char_traits<char>::eof())
             && (c!=terminator)) {
             s += c;
-            c = stream.get();
+            c = iStream.get();
         }
         return String(s.c_str());
     });
